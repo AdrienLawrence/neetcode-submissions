5ea61f9d62c32b182e8e0da2797568e6455f2d70
AdrienLawrence <firestar9jr@gmail.com>
2026-09-04 02:54:09 -0400

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,26 @@
+import math
+import copy
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        if not nums:
+            return [[]]
+
+        result = [[]]
+
+        for num in nums:
+
+            new_result = []
+
+            for sub in result:
+                for i in range(len(sub) + 1):
+                    temp = copy.deepcopy(sub)
+                    temp.insert(i, num)
+                    new_result.append(temp)
+
+            result = new_result
+                    
+
+
+        return result
